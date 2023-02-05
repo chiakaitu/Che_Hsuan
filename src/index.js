fb88fd3bd9ad07ca29445596cc403d08155625b2
chiakaitu <chiakaitu860914@gmail.com>
2023-02-05 23:45:37 +0800

    IT邦 React Day 14

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './assets/style.scss';
 import App from './App';
+import Progress from './Progress';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div className='container'>
+      <App />
+      <Progress />
+    </div>
   </React.StrictMode>
 );
 
